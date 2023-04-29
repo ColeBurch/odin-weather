4adc2a9fcf939e947d4685d4f4c03e9b385f770e
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-04-28 18:17:55 -0600

    Return data in an object.

--- src/index.js
+++ src/index.js
@@ -21,8 +21,31 @@ async function getWeather() {
     "http://api.weatherapi.com/v1/forecast.json?key=83eb9e8a5cef4f549f114636231704&q=Golden, Colorado&days=7&aqi=no&alerts=no"
   );
   const weatherData = await apiResponse.json();
+  try {
+    const locationName = weatherData["location"]["name"];
+    const regionName = weatherData["location"]["region"];
+    const currentDayForecast = weatherData["forecast"]["forecastday"]["0"];
+    const currentCondition = currentDayForecast["day"]["condition"]["text"];
+    const currentHighCelsius = currentDayForecast["day"]["maxtemp_c"];
+    const currentHighFarenheit = currentDayForecast["day"]["maxtemp_f"];
+    const currentLowCelsius = currentDayForecast["day"]["mintemp_c"];
+    const currentLowFarenheit = currentDayForecast["day"]["mintemp_f"];
+    return {
+      locationName,
+      regionName,
+      currentCondition,
+      currentHighCelsius,
+      currentHighFarenheit,
+      currentLowCelsius,
+      currentLowFarenheit,
+    };
+  } catch {}
+}
+
+function updateDashboard(weatherData) {
   console.log(weatherData);
 }
 
 document.body.appendChild(component());
-getWeather();
+let weatherData = getWeather();
+updateDashboard(weatherData);
